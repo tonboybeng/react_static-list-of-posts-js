dc05ae914482873758d5938f74fe1025c93f5987
Toni_Adrenalin <toni.toni@adrenalinmedia.com.au>
2025-04-29 12:33:59 +1000

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList';
 
 function getUserByUserId(userId) {
-  return usersFromServer.find(user => user.id === userId || null);
+  return usersFromServer.find(user => user.id === userId) || null;
 }
 
 function getCommentsByPostId(postId) {
